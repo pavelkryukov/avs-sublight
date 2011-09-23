cd8133108ab1b40e2f3a23c0a5b230ffe2bc6ad4
kryukov@frtk.ru <kryukov@frtk.ru>
2011-09-23 19:38:02 +0000

    New C# template for Client.

---
+++ sublight_cl_net/LampForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace sublight_cl_net
+{
+    public sealed class Lamp : Form
+    {   
+        public Lamp()
+        {
+            SuspendLayout();
+            //
+            // Lamp
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1292, 812);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            Name = "Lamp";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = @"Lamp";
+            Load += LampLoad;
+            MouseClick += LampMouseClick;
+            ResumeLayout(false);
+
+        }
+
+        private void LampMouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void LampLoad(object sender, EventArgs e)
+        {
+            Top = 0;
+            Left = 0;
+            Width = 5000;
+            Height = 5000;
+        }
+    }
+}
