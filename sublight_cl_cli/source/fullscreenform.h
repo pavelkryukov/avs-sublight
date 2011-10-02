c0a6b8b9969251ec423d68567725367d109bfabc
kryukov@frtk.ru <kryukov@frtk.ru>
2011-10-02 15:53:17 +0000

    CLI client removed to utils section

--- sublight_cl_cli/source/fullscreenform.h
+++
@@ -1,9 +0,0 @@
-
-ref class FullScreenForm : public System::Windows::Forms::Form {
-private:
-    bool _side;
-    bool _turn;
-public:
-    FullScreenForm(bool side);
-    System::Void Click(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e);
-};
