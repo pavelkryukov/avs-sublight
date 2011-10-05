4804e7cfad160e15733853f346f77cd250f0a5ca
kryukov@frtk.ru <kryukov@frtk.ru@b556164a-3005-4d10-08ff-b627bda9eb7f>
2011-10-05 17:41:59 +0000

    More refactoring of YUVtoRGB

--- trunk/as_sublight/source/sublight.cpp
+++ trunk/as_sublight/source/sublight.cpp
@@ -38,9 +38,9 @@ Sublight::~Sublight() {
  * Converter form YUV to RGB
 */
 unsigned __int32 Sublight::YUVtoRGB(unsigned __int32 Y, unsigned __int32 U, unsigned __int32 V) {
-    const signed __int32 R = (298 * ((signed _int32)Y - 16) + 409 *                                  ((signed _int32)V - 128) + 128) >> 8;
-    const signed __int32 G = (298 * ((signed _int32)Y - 16) - 100 * ((signed _int32)U - 128) - 208 * ((signed _int32)V - 128) + 128) >> 8;
-    const signed __int32 B = (298 * ((signed _int32)Y - 16) + 516 * ((signed _int32)U - 128) + 128)                                  >> 8;
+    const signed __int32 R = (298 * ((signed __int32)Y - 16) + 409 *                                   ((signed __int32)V - 128) + 128) >> 8;
+    const signed __int32 G = (298 * ((signed __int32)Y - 16) - 100 * ((signed __int32)U - 128) - 208 * ((signed __int32)V - 128) + 128) >> 8;
+    const signed __int32 B = (298 * ((signed __int32)Y - 16) + 516 * ((signed __int32)U - 128)                                   + 128) >> 8;
   
     unsigned __int32 out = 0x00000000;
     out += (CUT(R) << 8) + (CUT(G) << 16) + (CUT(R) << 24);
