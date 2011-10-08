6630e14fdb634a8fcc7cbfe18699011b78d9aaf5
kryukov@frtk.ru <kryukov@frtk.ru>
2011-10-08 12:21:10 +0000

    [Filter] Code style according to cpplint.py [Filter] Macros changed to static inline functions

--- as_sublight/source/sublightUDP.cpp
+++ as_sublight/source/sublightUDP.cpp
@@ -3,29 +3,31 @@
  *
  * Sublight filter with UDP socket transmission.
  *
- * Copyright 2011 Kryukov Pavel.
+ * Copyright 2011 Pavel Kryukov (C)
 */
 
 #pragma comment(lib, "Ws2_32.lib")
 
-#include "sublightUDP.h"
+#include "./sublightUDP.h"
 
-SublightUDP::SublightUDP(PClip child, unsigned __int16 port, const char* const ip) : Sublight(child) {
+SublightUDP::SublightUDP(PClip child,
+                         unsigned __int16 port,
+                         const char* const ip) : Sublight(child) {
     WSADATA wsadata = {};
-    WSAStartup (MAKEWORD (2, 2), &wsadata);
+    WSAStartup(MAKEWORD(2, 2), &wsadata);
 
     this->_sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
- 
+
     dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port   = htons( port);
-    dest_addr.sin_addr.s_addr = inet_addr( ip);
-    
+    dest_addr.sin_port   = htons(port);
+    dest_addr.sin_addr.s_addr = inet_addr(ip);
+
     BOOL bOptVal = TRUE;
     setsockopt(_sd, SOL_SOCKET, SO_BROADCAST, (char*)&bOptVal, sizeof(BOOL));
 }
 
 SublightUDP::~SublightUDP() {
-    closesocket (this->_sd);
+    closesocket(this->_sd);
 }
 
 void SublightUDP::Send(unsigned __int64 data) {
--- as_sublight/source/sublightUDP.h
+++ as_sublight/source/sublightUDP.h
@@ -3,26 +3,26 @@
  *
  * Sublight filter with UDP socket transmission.
  *
- * Copyright 2011 Kryukov Pavel.
+ * Copyright 2011 Pavel Kryukov (C)
 */
 
-#ifndef __SUBLIGHT_UDP_H_
-#define __SUBLIGHT_UDP_H_
+#ifndef AS_SUBLIGHT_SOURCE_SUBLIGHTUDP_H_
+#define AS_SUBLIGHT_SOURCE_SUBLIGHTUDP_H_
 
 // Windows socket header
 #include <WinSock.h>
 
 // AviSynth SDK
-#include "sublight.h"
+#include "./sublight.h"
 
 class SublightUDP : public Sublight {
   private:
     SOCKET _sd;
-	sockaddr_in dest_addr;
+    sockaddr_in dest_addr;
     virtual void Send(unsigned __int64 data);
   public:
     SublightUDP(PClip child, const unsigned __int16 port, const char* const ip);
     ~SublightUDP();
 };
 
-#endif  // __SUBLIGHT_UDP_H_
+#endif  // AS_SUBLIGHT_SOURCE_SUBLIGHTUDP_H_
