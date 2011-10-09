3acad5b6d48fc3e62779045226f5b671ddc953bd
kryukov@frtk.ru <kryukov@frtk.ru>
2011-10-09 19:58:29 +0000

    [Filter] Types.h attach (sorry)

---
+++ as_sublight/source/types.h
@@ -0,0 +1,18 @@
+/*
+ * types.h
+ *
+ * AVS filter type defines
+ *
+ * Copyright 2011 Kryukov Pavel.
+*/
+
+#ifndef AS_SUBLIGHT_SOURCE_TYPES_H_
+#define AS_SUBLIGHT_SOURCE_TYPES_H_
+
+typedef unsigned __int8 pixel;
+typedef unsigned __int8 bpp_t;
+typedef unsigned __int32 uint32;
+typedef signed __int32 sint32;
+typedef unsigned __int64 uint64;
+
+#endif  // AS_SUBLIGHT_SOURCE_TYPES_H_
