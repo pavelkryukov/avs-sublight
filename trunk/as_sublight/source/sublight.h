ee1409fd55cbd45abd9674866f82ae6d6b774884
kryukov@frtk.ru <kryukov@frtk.ru@b556164a-3005-4d10-08ff-b627bda9eb7f>
2011-10-23 19:32:07 +0000

    [Client] Code style corrections

--- trunk/as_sublight/source/sublight.h
+++ trunk/as_sublight/source/sublight.h
@@ -22,10 +22,10 @@ class Sublight : public GenericVideoFilter {
   private:
     // Signature of sender
     static const coord_t SIGNATURE = 0xF0;
-    
+
     // Trim 4-byte number to 1-byte
     static inline uint32 CUTS(sint32 a) {
-        return (a > 0xFF) ? 0xFF : (a < 0x00 ? 0x00 : (uint32)a);
+        return (a > 0xFF) ? 0xFF : (a < 0x00 ? 0x00 : static_cast<uint32>(a));
     }
 
     // YUV to RGB converter
@@ -36,7 +36,7 @@ class Sublight : public GenericVideoFilter {
     void SetSizesIL24(const PVideoFrame src);
     void SetSizesIL32(const PVideoFrame src);
     void SetSizesYV12(const PVideoFrame src);
-    
+
     // Sizes
     unsigned width;
     unsigned height;
@@ -53,18 +53,19 @@ class Sublight : public GenericVideoFilter {
     uint32(Sublight::*const _getAv)(const PVideoFrame src, coord_t xy) const;
     uint32 GetAvRGB24(const PVideoFrame src, coord_t xy) const;
     uint32 GetAvRGB32(const PVideoFrame src, coord_t xy) const;
-    uint32 GetAvYUY2 (const PVideoFrame src, coord_t xy) const;
-    uint32 GetAvYV12 (const PVideoFrame src, coord_t xy) const;
+    uint32 GetAvYUY2(const PVideoFrame src, coord_t xy) const;
+    uint32 GetAvYV12(const PVideoFrame src, coord_t xy) const;
 
     virtual void Send(uint32 data) const = 0;
 
     static const coord_t frames[];
+    static const unsigned framesAmount;
   public:
     explicit Sublight(PClip child);
 
     // DO NOT make this method 'const'
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
-	
+
     virtual __stdcall ~Sublight() {}
 };
 
