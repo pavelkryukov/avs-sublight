9e0cddc9fa7c888ffb752cbc8c832c11514317af
kryukov@frtk.ru <kryukov@frtk.ru>
2011-10-24 15:43:07 +0000

    [Filter] Type- and this-correctness

--- as_sublight/source/sublight.h
+++ as_sublight/source/sublight.h
@@ -24,48 +24,57 @@ class Sublight : public GenericVideoFilter {
     static const coord_t SIGNATURE = 0xF0;
 
     // Trim 4-byte number to 1-byte
-    static inline uint32 CUTS(sint32 a) {
-        return (a > 0xFF) ? 0xFF : (a < 0x00 ? 0x00 : static_cast<uint32>(a));
+    static inline packet_t CUTS(signed __int32 a) {
+        return (a > 0xFF) ? 0xFF : 
+               (a < 0x00) ? 0x00 : static_cast<packet_t>(a);
     }
 
     // YUV to RGB converter
-    static uint32 YuvToRgb(uint32 Y, uint32 U, uint32 V);
+    static packet_t YuvToRgb(average_t Y, average_t U, average_t V);
 
-    // Size setters
-    void(Sublight::*const _setSizes)(const PVideoFrame src);
-    void SetSizesIL24(const PVideoFrame src);
-    void SetSizesIL32(const PVideoFrame src);
+    // Size setter
+    inline void SetSizes(const PVideoFrame src);
+
+    // Size setter for different colorspaces
+    void(Sublight::*const _setSizesBpp)(const PVideoFrame src);
+    void SetSizesRGB24(const PVideoFrame src);
+    void SetSizesRGB32(const PVideoFrame src);
+    void SetSizesYUY2(const PVideoFrame src);
     void SetSizesYV12(const PVideoFrame src);
 
     // Sizes
-    unsigned width;
-    unsigned height;
-    unsigned line;
-    unsigned averageSize;
+    fsize_t width;
+    fsize_t height;
+    fsize_t line;
+    fsize_t averageSize;
 
     // UV sizes
-    unsigned widthUV;
-    unsigned heightUV;
-    unsigned lineUV;
-    unsigned averageSizeUV;
+    fsize_t widthUV;
+    fsize_t heightUV;
+    fsize_t lineUV;
+    fsize_t averageSizeUV;
 
     // Average counters
-    uint32(Sublight::*const _getAv)(const PVideoFrame src, coord_t xy) const;
-    uint32 GetAvRGB24(const PVideoFrame src, coord_t xy) const;
-    uint32 GetAvRGB32(const PVideoFrame src, coord_t xy) const;
-    uint32 GetAvYUY2(const PVideoFrame src, coord_t xy) const;
-    uint32 GetAvYV12(const PVideoFrame src, coord_t xy) const;
+    packet_t(Sublight::*const _getAv)(const PVideoFrame src, coord_t xy) const;
+    packet_t GetAvRGB24(const PVideoFrame src, coord_t xy) const;
+    packet_t GetAvRGB32(const PVideoFrame src, coord_t xy) const;
+    packet_t GetAvYUY2(const PVideoFrame src, coord_t xy) const;
+    packet_t GetAvYV12(const PVideoFrame src, coord_t xy) const;
 
-    virtual void Send(uint32 data) const = 0;
+    // Sending method (virtual)
+    virtual void Send(packet_t data) const = 0;
 
+    // Working frames array
     static const coord_t frames[];
-    static const unsigned framesAmount;
+    static const sectorNum_t framesAmount;
   public:
+    // Constructor
     explicit Sublight(PClip child);
 
     // DO NOT make this method 'const'
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 
+    // Destructor
     virtual __stdcall ~Sublight() {}
 };
 
--- as_sublight/source/sublightUDP.h
+++ as_sublight/source/sublightUDP.h
@@ -22,9 +22,9 @@ class SublightUDP : public Sublight {
   private:
     SOCKET _sd;
     sockaddr_in dest_addr;
-    virtual void Send(uint32 data) const;
+    virtual void Send(packet_t data) const;
   public:
-    SublightUDP(PClip child, const unsigned __int16 port, const char* const ip);
+    SublightUDP(PClip child, const port_t port, const char* const ip);
     virtual _stdcall ~SublightUDP();
 };
 
