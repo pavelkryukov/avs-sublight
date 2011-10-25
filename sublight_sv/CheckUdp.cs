c3e2878ac4e7b3ab28ce6708b7b804c1df78e02f
kryukov@frtk.ru <kryukov@frtk.ru>
2011-10-25 22:19:24 +0000

    [Server] Some simplifications.

--- sublight_sv/CheckUdp.cs
+++ sublight_sv/CheckUdp.cs
@@ -3,7 +3,7 @@ using System.Net.Sockets;
 
 namespace sublight_sv
 {
-    internal sealed class CheckUdp : Check
+    internal sealed class CheckUdp : Check, System.IDisposable
     {
         private readonly Socket _mysocket; 
         private readonly IPEndPoint _sender;
@@ -36,7 +36,12 @@ namespace sublight_sv
             _sender = new IPEndPoint(IPAddress.Broadcast, port);
         }
 
-        public void KillSocket()
+        public void Dispose()
+        {
+            _mysocket.Close();
+        }
+
+        ~CheckUdp()
         {
             _mysocket.Close();
         }
